ebb2b4c9e170cf65065ce8d5c1065b187181d8d2
Andrey Filimonov <andrfil05@mail.ru>
2026-04-06 19:40:10 +0600

    add inject annotation

--- app/src/main/java/com/filimonov/todolist/domain/usecase/GetAllTodoForDateUseCase.kt
+++ app/src/main/java/com/filimonov/todolist/domain/usecase/GetAllTodoForDateUseCase.kt
@@ -4,8 +4,9 @@ import com.filimonov.todolist.domain.entity.Todo
 import com.filimonov.todolist.domain.repository.TodoRepository
 import kotlinx.coroutines.flow.Flow
 import java.time.LocalDate
+import javax.inject.Inject
 
-class GetAllTodoForDateUseCase(private val repository: TodoRepository) {
+class GetAllTodoForDateUseCase @Inject constructor(private val repository: TodoRepository) {
 
     operator fun invoke(date: LocalDate): Flow<List<Todo>> {
         return repository.getAllTodoForDate(date)
