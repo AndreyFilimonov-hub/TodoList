2b2f2e1cb091ba9d116f64870a004caa6c898498
Andrey Filimonov <andrfil05@mail.ru>
2026-04-06 19:28:04 +0600

    create TodoRepositoryImpl

---
+++ app/src/main/java/com/filimonov/todolist/data/repository/TodoRepositoryImpl.kt
@@ -0,0 +1,25 @@
+package com.filimonov.todolist.data.repository
+
+import com.filimonov.todolist.data.dao.TodoDao
+import com.filimonov.todolist.data.mapper.getEndDayTimeMillis
+import com.filimonov.todolist.data.mapper.getStartDayTimeMillis
+import com.filimonov.todolist.data.mapper.toDbModel
+import com.filimonov.todolist.data.mapper.toEntities
+import com.filimonov.todolist.domain.entity.Todo
+import com.filimonov.todolist.domain.repository.TodoRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import java.time.LocalDate
+
+class TodoRepositoryImpl(
+    private val todoDao: TodoDao
+) : TodoRepository {
+    override fun getAllTodoForDate(date: LocalDate): Flow<List<Todo>> {
+        return todoDao.getAllTodoForDate(date.getStartDayTimeMillis(), date.getEndDayTimeMillis())
+            .map { it.toEntities() }
+    }
+
+    override suspend fun addTodo(todo: Todo) {
+        todoDao.addTodo(todo.toDbModel())
+    }
+}
