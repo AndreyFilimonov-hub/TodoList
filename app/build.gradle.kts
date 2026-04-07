993ca5015ca1b2529d9af8435653355b483cc23d
Andrey Filimonov <andrfil05@mail.ru>
2026-04-07 22:49:27 +0600

    add navigation dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,8 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.navigation.compose)
+
     implementation(libs.hilt.android)
     ksp(libs.hilt.android.compiler)
     implementation(libs.androidx.hilt.navigation.compose)
